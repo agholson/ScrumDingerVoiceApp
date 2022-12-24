7f077cce2177bbda7974e9e633626a61865431b6
agholson <87455899+agholson@users.noreply.github.com>
2022-12-24 09:33:04 -0500

    Initial Commit

---
+++ ScrumDingerVoiceApp/ScrumDingerVoiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerVoiceAppApp.swift
+//  ScrumDingerVoiceApp
+//
+//  Created by Leone on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerVoiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
